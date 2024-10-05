c27517acb94de55b8284052566acb9d1a4b839b0
117marta <117marta@gmail.com>
2024-10-05 22:03:16 +0200

    Install pyest + test home page

--- home_page/tests.py
+++ home_page/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestHomePage(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse("home-page")
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_should_return_200(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
